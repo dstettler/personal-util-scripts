1fba0eb4303cb8df577dfcd9dfc25cb91f8a5378
Devon <devon.stettler@tutanota.com>
2024-11-03 19:51:06 -0500

    Update python build script for PS7

--- .github/pybuild.ps1
+++ .github/pybuild.ps1
@@ -8,6 +8,8 @@ if ($null -eq $args[0]) {
     $archiveTag = $args[0]
 }
 
+$pyreleaseName = "python-bins-$archiveTag.zip"
+
 # Build simple utils
 Get-ChildItem "." -Filter *.py | 
 ForEach-Object {
@@ -16,10 +18,11 @@ ForEach-Object {
 
 Get-ChildItem "dist" | 
 ForEach-Object {
-    $distpath = $_
+    $distpath = $(Split-Path -Path $_ -Leaf)
     Get-ChildItem dist\$distpath -Filter *.exe | 
     ForEach-Object {
-        Copy-Item dist\$distpath\$_ pyrelease\$_
+        $filename = $(Split-Path -Path $_ -Leaf)
+        Copy-Item dist\$distpath\$filename pyrelease\$filename
     }
 }
 
@@ -31,15 +34,17 @@ Remove-Item dist -Recurse -Force
 mkdir pyrelease\pin-to-all-apps
 Get-ChildItem "pin-to-all-apps" -Filter *.py | 
 ForEach-Object {
-    pyinstaller .\pin-to-all-apps\$_
+    $pyFile = $(Split-Path -Path $_ -Leaf)
+    pyinstaller .\pin-to-all-apps\$pyFile
 }
 
 Get-ChildItem "dist" | 
 ForEach-Object {
-    $distpath = $_
+    $distpath = $(Split-Path -Path $_ -Leaf)
     Get-ChildItem dist\$distpath -Filter *.exe | 
     ForEach-Object {
-        Copy-Item dist\$distpath\$_ pyrelease\pin-to-all-apps\$_
+        $filename = $(Split-Path -Path $_ -Leaf)
+        Copy-Item dist\$distpath\$filename pyrelease\pin-to-all-apps\$filename
     }
 }
 
@@ -52,10 +57,10 @@ Remove-Item dist -Recurse -Force
 $compress = @{
     Path = ".\pyrelease\*"
     CompressionLevel = "Optimal"
-    DestinationPath = ".\pyrelease-$archiveTag.zip"
+    DestinationPath = ".\$pyreleaseName"
 }
 Compress-Archive @compress
 
 Remove-Item pyrelease -Recurse -Force 
 
-Copy-Item "python-bins-$archiveTag.zip" release
+Copy-Item $pyreleaseName release
