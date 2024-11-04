f3847e401f78021271231ff9bbc41fda774a9958
Devon <me@dstet.me>
2024-11-03 20:08:12 -0500

    Implement CI/CD on utils (#1)
    * Start work on CI/CD
    
    * Add build scripts
    
    * Update python build script for PS7

---
+++ .github/pybuild.ps1
@@ -0,0 +1,64 @@
+mkdir pyrelease
+
+$archiveTag = ""
+
+if ($null -eq $args[0]) {
+    $archiveTag = $(git rev-parse HEAD)
+} else {
+    $archiveTag = $args[0]
+}
+
+$pyreleaseName = "python-bins-$archiveTag.zip"
+
+# Build simple utils
+Get-ChildItem "." -Filter *.py | 
+ForEach-Object {
+    pyinstaller $_
+}
+
+Get-ChildItem "dist" | 
+ForEach-Object {
+    $distpath = $(Split-Path -Path $_ -Leaf)
+    Get-ChildItem dist\$distpath -Filter *.exe | 
+    ForEach-Object {
+        $filename = $(Split-Path -Path $_ -Leaf)
+        Copy-Item dist\$distpath\$filename pyrelease\$filename
+    }
+}
+
+# Delete simple util intermediary files
+Remove-Item build -Recurse -Force 
+Remove-Item dist -Recurse -Force 
+
+# Build pin-to-all-apps
+mkdir pyrelease\pin-to-all-apps
+Get-ChildItem "pin-to-all-apps" -Filter *.py | 
+ForEach-Object {
+    $pyFile = $(Split-Path -Path $_ -Leaf)
+    pyinstaller .\pin-to-all-apps\$pyFile
+}
+
+Get-ChildItem "dist" | 
+ForEach-Object {
+    $distpath = $(Split-Path -Path $_ -Leaf)
+    Get-ChildItem dist\$distpath -Filter *.exe | 
+    ForEach-Object {
+        $filename = $(Split-Path -Path $_ -Leaf)
+        Copy-Item dist\$distpath\$filename pyrelease\pin-to-all-apps\$filename
+    }
+}
+
+# Delete pin-to-all-apps intermediary files
+Remove-Item build -Recurse -Force 
+Remove-Item dist -Recurse -Force 
+
+$compress = @{
+    Path = ".\pyrelease\*"
+    CompressionLevel = "Optimal"
+    DestinationPath = ".\$pyreleaseName"
+}
+Compress-Archive @compress
+
+Remove-Item pyrelease -Recurse -Force 
+
+Copy-Item $pyreleaseName release
